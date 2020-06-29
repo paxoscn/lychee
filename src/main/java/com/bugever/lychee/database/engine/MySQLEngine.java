df86e1deb4651455b45e66e397882e064614afc3
paxoscn <unrealwalker@126.com>
2020-06-29 19:31:20 +0800

    dimensions

--- src/main/java/com/bugever/lychee/database/engine/MySQLEngine.java
+++ src/main/java/com/bugever/lychee/database/engine/MySQLEngine.java
@@ -1,16 +1,24 @@
 package com.bugever.lychee.database.engine;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLEngine implements Engine {
 
+    private final String url;
+    private final String user;
+    private final String password;
+
     public MySQLEngine(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
     }
 
     @Override
-    public Connection getConnection() {
-        return null;
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
     }
 
     @Override
